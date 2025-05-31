b6164739e2b7638913ca05a1594c5f26c9e87da3
Bero898 <90900678+Bero898@users.noreply.github.com>
2025-05-31 17:28:36 +0200

    cleaning up

--- run_metrics/average.py
+++ run_metrics/average.py
@@ -17,7 +17,9 @@ product_numbers = {
 product_names = list(product_numbers.keys())
 
 # Directories for each metric
-base_dir = "./GEvalTest"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+base_dir = os.path.join(BASE_DIR, "../GEvalTest")
 dirs = {
     "BLEU": "bleu_results",
     "BERTScore": "bertscore_results",
